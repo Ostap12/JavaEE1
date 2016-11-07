94d12c267f10f2402e47bc5cff4852e1eaf12e35
Ostap12 <ostapbor@gmail.com>
2016-11-07 05:51:40 +0200

    created Spring project

---
+++ src/main/java/ua/com/teaspo/test/HelloW/App.java
@@ -0,0 +1,17 @@
+package ua.com.teaspo.test.HelloW;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBean.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+		obj.printHello();
+
+	}
+}
